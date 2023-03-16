6cd29067819c711c91366effb3e017a9c747938c
alafaethy Gomes <alafaethy@gmail.com>
2023-03-16 17:32:42 -0300

    Add: ancor ability e js responsive menu of click

--- static/script.js
+++ static/script.js
@@ -11,3 +11,17 @@ menuMobile.addEventListener("click", () => {
     : menuMobile.classList.replace("bi-x","bi-list")
     body.classList.toggle("menu-nav-active")
 })
+
+
+// Fechar o menu clicar em algum item e mudar o icone 
+
+
+const navItem = document.querySelectorAll(".nav-item")
+navItem.forEach(item =>{
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x","bi-list")
+        }
+    })
+})
